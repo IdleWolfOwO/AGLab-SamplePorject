c9f654fa485aac82f1c331652baa90f268ec68a5
IdleWolf <3466503232@qq.com>
2025-09-11 18:57:44 +0800

    Class2 Homework

--- Assets/课程文件/第二课 物体简单逻辑/课后练习/Class2_CubeRot_answer.cs
+++ Assets/课程文件/第二课 物体简单逻辑/课后练习/Class2_CubeRot_answer.cs
@@ -2,8 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Class2_CubeRot : MonoBehaviour
+public class Class2_CubeRot_answer : MonoBehaviour
 {
+    // 速率控制
+    // 使用 Public 公开的基础变量可以在检查器面板上控制
+    public float Rate = 20f;
+
+
     // 类变量，存储要转动的物体
     private GameObject Cube;
 
@@ -15,12 +20,14 @@ public class Class2_CubeRot : MonoBehaviour
         Cube = this.gameObject;
     }
 
-
-    private void FixedUpdate()
+    void FixedUpdate()
     {
         // 在每一帧旋转物体
         // Time.deltaTime => 距离上一帧经过的时间
         // 每秒旋转 * 这两帧经过多少秒（Time.deltaTime） => 这一帧要转多大
-        Cube.transform.eulerAngles += new Vector3(0, 20f, 0) * Time.deltaTime;
+        Cube.transform.eulerAngles += new Vector3(0, Rate, 0) * Time.deltaTime;
+
+        // 在 Rate 前加上一个 “-” 表示向相反方向转动
+        // Cube.transform.eulerAngles += new Vector3(0, -Rate, 0) * Time.deltaTime;
     }
 }
